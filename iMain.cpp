21ec92fed067bdae438bd17f9bff1a2e563a9bb9
Sayeeb Rahman <sayeeb2001@gmail.com>
2025-06-26 08:58:02 +0600

    [feat]: added enemy sys, refactored kinematics

--- iMain.cpp
+++ iMain.cpp
@@ -1,12 +1,14 @@
 #include <math.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <time.h>
 
 #include "iGraphics.h"
 
 #define PI 3.14159265
 #define MAX_SIZE 10
 #define ESC 0x1b
+#define NUM_ENEMIES 1
 
 typedef enum {
     STATE_MENU,
@@ -16,7 +18,9 @@ typedef enum {
 
 typedef enum {
     TYPE_BLOCK,
-    TYPE_PLAYER
+    TYPE_PLAYER,
+    TYPE_ENEMY,
+    TYPE_NULL
 } object_t;
 
 typedef struct {
@@ -29,6 +33,7 @@ typedef struct {
     uint8_t r;
     uint8_t g;
     uint8_t b;
+    uint8_t a;
 } color_t;
 
 typedef struct {
@@ -42,6 +47,13 @@ typedef enum {
     LOOK_UP,
     LOOK_DOWN
 } look_t;
+
+typedef enum {
+    ENEMY_COILY,
+    ENEMY_UGG,
+    ENEMY_WRONGWAY,
+    ENEMY_SAM
+} enemytype_t;
 
 typedef struct {
     double x, y, z;
@@ -61,9 +73,19 @@ typedef struct {
 } body_t;
 
 typedef struct {
+    body_t km;
+} player_t;
+
+typedef struct {
+    body_t km;
+    enemytype_t type;
+} enemy_t;
+
+typedef struct {
     object_t type;
     position_t pos;
     uint8_t flags;
+    void *ref;
 } drawqueue_t;
 
 app_t app_state = STATE_MENU;
@@ -72,15 +94,17 @@ editor_t editor = {.wireframe=false,.grid=false,.debug=false};
 
 tile_t tiles[MAX_SIZE][MAX_SIZE][MAX_SIZE];
 
-drawqueue_t drawqueue[MAX_SIZE*MAX_SIZE*MAX_SIZE+1];
+drawqueue_t drawqueue[MAX_SIZE*MAX_SIZE*MAX_SIZE+1+NUM_ENEMIES+1];
 
-body_t player;
+player_t player;
+
+enemy_t enemies[NUM_ENEMIES];
 
 int width=800,height=800;
 
 double start_x=width/2;
 double start_y=height*0.9;
-double a = 50;
+double a = 30;
 
 double blocksPos3d[][3]={
     {7,7,0},{6,7,1},{5,7,2},{4,7,3},{3,7,4},{2,7,5},{1,7,6},{0,7,7},
@@ -99,6 +123,10 @@ double blocksPos3d[][3]={
     {3,7,5}
 };
 
+color_t coily={.r=10,.g=100,.b=240};
+color_t ugg={.r=100,.g=200,.b=150};
+color_t sam={.r=10,.g=255,.b=200};
+
 int n =  sizeof(blocksPos3d)/sizeof(blocksPos3d[0]);
 
 int cmp_dk (const void *a, const void *b) {
@@ -107,6 +135,13 @@ int cmp_dk (const void *a, const void *b) {
     int d1 = A->pos.x+A->pos.z+(MAX_SIZE-1-A->pos.y)+A->type;
     int d2 = B->pos.x+B->pos.z+(MAX_SIZE-1-B->pos.y)+B->type;
     return (d1>d2)-(d1<d2);
+}
+
+void iClearQueue() {
+    for(int i = 0; i < MAX_SIZE*MAX_SIZE*MAX_SIZE+1;i++)
+    drawqueue[i].type=TYPE_NULL,
+    drawqueue[i].pos.x=0,drawqueue[i].pos.y=0,drawqueue[i].pos.z=0,
+    drawqueue[i].flags=0;
 }
 
 void iTile(double x, double y) {
@@ -139,53 +174,48 @@ void iSideOutline(double x, double y) {
     iPolygon(x_coords,y_coords,4);
 }
 
+void iDrawEnemy(enemy_t* enemy) {
+    switch(enemy->type) {
+        case ENEMY_COILY: iSetColor(coily.r,coily.g,coily.b);break;
+        case ENEMY_UGG: iSetColor(ugg.r,ugg.g,ugg.b);break;
+        case ENEMY_SAM: iSetColor(sam.r,sam.g,sam.b);break;
+        default: iSetColor(200,210,100);break;
+    }
+    // printf("%d %d %d\n",(int)enemy->km.pos.x,(int)enemy->km.pos.y,(int)enemy->km.pos.z);
+    iFilledCircle(start_x+(enemy->km.pos.z-enemy->km.pos.x)*a*cos(PI/6),start_y-(enemy->km.pos.z+enemy->km.pos.x)*a/2-enemy->km.pos.y*a-a/2,a/5);
+}
+
 void iDrawQueue() {
-    // for(int s=0; s<MAX_SIZE*2; s++) {
-    //     for(int x=0; x<=s&&x<MAX_SIZE; x++) {
-    //         int z = s-x;
-    //         if (z>=MAX_SIZE)continue;
-    //         for(int y =MAX_SIZE-1;y>=0;y--) {
-    //             if (tiles[y][x][z].valid) {
-    //                 if (!editor.wireframe) {
-    //                     iSetColor(86, 70, 239);
-    //                     iTile(start_x+(z-x)*a*cos(PI/6),start_y-(z+x)*a/2-y*a);
-    //                     iSide(start_x+(z-x)*a*cos(PI/6),start_y-(z+x)*a/2-y*a);
-    //                 } else {
-    //                     iSetColor(49, 70, 70);
-    //                     iTileOutline(start_x+(z-x)*a*cos(PI/6),start_y-(z+x)*a/2-y*a);
-    //                     iSideOutline(start_x+(z-x)*a*cos(PI/6),start_y-(z+x)*a/2-y*a);
-    //                 }
-    //                 if (player.pos.x==x&&player.pos.y==y&&player.pos.z==z) {
-    //                     iSetTransparentColor(0,0,0,0.5);
-    //                     iFilledEllipse(start_x+(z-x)*a*cos(PI/6),start_y-(z+x)*a/2-y*a-a/2,a/4,a/8);
-    //                     iSetColor(240,10,10);
-    //                     iFilledCircle(start_x+(z-x)*a*cos(PI/6),start_y-(z+x)*a/2-y*a-a/2,a/3);
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
     // current approach: use a draw queue
-    int i = 0;
+    iClearQueue();
+    int i = 0,j;
     for(int y=MAX_SIZE-1;y>=0;y--) {
         for(int x = 0; x < MAX_SIZE;x++) {
             for(int z = 0; z < MAX_SIZE;z++) {
                 if (!tiles[y][x][z].valid) continue;
                 drawqueue[i].pos.x=x,drawqueue[i].pos.y=y,drawqueue[i].pos.z=z;
                 drawqueue[i].flags=tiles[y][x][z].state;
+                drawqueue[i].ref=&tiles[y][x][z];
                 drawqueue[i++].type=TYPE_BLOCK;
             }
         }
     }
     if (app_state==STATE_GAME) {
-        drawqueue[i].pos.x=player.pos.x,drawqueue[i].pos.y=player.pos.y,drawqueue[i].pos.z=player.pos.z;
-        drawqueue[i].flags=player.jump.active;
+        drawqueue[i].pos.x=player.km.pos.x,drawqueue[i].pos.y=player.km.pos.y,drawqueue[i].pos.z=player.km.pos.z;
+        drawqueue[i].flags=player.km.jump.active;
+        drawqueue[i].ref=&player;
         drawqueue[i++].type=TYPE_PLAYER;
+        for(j=0;j<NUM_ENEMIES;j++) {
+            drawqueue[i].pos.x=enemies[j].km.pos.x,drawqueue[i].pos.y=enemies[j].km.pos.y,drawqueue[i].pos.z=enemies[j].km.pos.z;
+            drawqueue[i].flags=enemies[j].type;
+            drawqueue[i].ref=&enemies[j];
+            drawqueue[i++].type=TYPE_ENEMY;
+        }
     }
     qsort(drawqueue, i, sizeof(drawqueue_t),cmp_dk);
     // draw them now
     // for(int k = 0; k < i; k++) printf("p: %lf %lf %lf f: %d t: %d\n",drawqueue[k].x,drawqueue[k].y,drawqueue[k].z,drawqueue[k].flags,drawqueue[k].type);
-    for(int j=0; j<i;j++) {
+    for(j=0; j<i;j++) {
         double x=drawqueue[j].pos.x,y=drawqueue[j].pos.y,z=drawqueue[j].pos.z;
         switch(drawqueue[j].type) {
             case TYPE_BLOCK: {
@@ -207,6 +237,11 @@ void iDrawQueue() {
                 iFilledCircle(start_x+(z-x)*a*cos(PI/6),start_y-(z+x)*a/2-y*a-a/2,a/3);
                 break;
             }
+            case TYPE_ENEMY: {
+                iDrawEnemy((enemy_t*)drawqueue[j].ref);
+                break;
+            }
+            default: break;
         }
     }
 }
@@ -239,50 +274,83 @@ void iBlock() {
 }
 
 void iPlayer() {
-    player.pos.x=0;
-    player.pos.y=0;
-    player.pos.z=1;
-    player.jump.active=0;
+    player.km.pos.x=0;
+    player.km.pos.y=0;
+    player.km.pos.z=1;
+    player.km.jump.active=0;
 }
 
-void iPlayerMove(int x, int y, int z) {
+int iBodyMove(int x, int y, int z, body_t *km) {
     printf("%d %d %d\n",x,y,z);
     if (!(x>=0&&x<MAX_SIZE&&y>=0&&y<MAX_SIZE&&z>=0&&z<MAX_SIZE)) {
         // die and reset
         printf("so, %d %d %d is invalid\n",x,y,z);
-        return;
+        return 0;
     }
     if ((y-1>=0&&tiles[y-1][x][z].valid) && 1) {
         printf("so, up?\n");
         if (y-2>=-1&&!tiles[y-2][x][z].valid) {
             printf("so, up one block actually?\n");
-            player.pos.x=x,player.pos.y=y-1,player.pos.z=z;
-            printf("%lf %lf %lf\n",player.pos.x,player.pos.y,player.pos.z);
+            km->pos.x=x,km->pos.y=y-1,km->pos.z=z;
+            printf("%lf %lf %lf\n",km->pos.x,km->pos.y,km->pos.z);
         }
         // then go, otherwise stay where you are
     } else if (tiles[y][x][z].valid) {
         // simply walk to this one, with no jump anim
         printf("so walk straight?\n");
-        player.pos.x=x,player.pos.y=y,player.pos.z=z;
+        km->pos.x=x,km->pos.y=y,km->pos.z=z;
     } else if (y+1<=MAX_SIZE&&tiles[y+1][x][z].valid) {
         // then move to this one, still a jump anim
         printf("so down one block?\n");
-        player.pos.x=x,player.pos.y=y+1,player.pos.z=z;
+        km->pos.x=x,km->pos.y=y+1,km->pos.z=z;
     } else {
         // dont move
         printf("huh? no move? thats boring...\n");
-        return;
+        return 0;
     }
     // now initiate movement and jump animation
     printf("time to go... yay!!!\n");
-    printf("%lf %lf %lf\n",player.pos.x,player.pos.y,player.pos.z);
-    return;
+    printf("%lf %lf %lf\n",km->pos.x,km->pos.y,km->pos.z);
+    return 1;
 }
+
+void iEnemyStep() {
+    for(int i = 0; i < NUM_ENEMIES; i++) {
+        switch(enemies[i].type) {
+            case ENEMY_COILY: case ENEMY_SAM: case ENEMY_UGG: case ENEMY_WRONGWAY:
+            default: {
+                for(int j = 0; i < 4; j++) {
+                    int d = rand()%4,s;
+                    switch(d) {
+                        case 0: s=iBodyMove(enemies[i].km.pos.x, enemies[i].km.pos.y,enemies[i].km.pos.z-1,&enemies[i].km); break;
+                        case 1: s=iBodyMove(enemies[i].km.pos.x, enemies[i].km.pos.y,enemies[i].km.pos.z+1,&enemies[i].km); break;
+                        case 2: s=iBodyMove(enemies[i].km.pos.x-1, enemies[i].km.pos.y,enemies[i].km.pos.z,&enemies[i].km); break;
+                        case 3: s=iBodyMove(enemies[i].km.pos.x+1, enemies[i].km.pos.y,enemies[i].km.pos.z,&enemies[i].km); break;
+                        default: break;
+                    }
+                    if (s) break;
+                }
+                break;
+            }
+        }
+        printf("here we go: %lf %lf %lf\n",enemies[i].km.pos.x,enemies[i].km.pos.y,enemies[i].km.pos.z);
+    }
+}
+
+void iEnemy() {
+    for(int i = 0; i < NUM_ENEMIES; i++) {
+        enemies[i].type=ENEMY_COILY;
+        enemies[i].km.pos.x=0,enemies[i].km.pos.y=0,enemies[i].km.pos.z=0;
+    }
+}
+
 
 void iGame() {
     app_state=STATE_GAME;
     iBlock();
     iPlayer();
+    iEnemy();
+    iSetTimer(1000, iEnemyStep);
     // drawqueue[i].pos.x=player.pos.x,drawqueue[i].pos.y=player.pos.y,drawqueue[i].pos.z=player.pos.z;
     // drawqueue[i].flags=0;
     // drawqueue[i].type=TYPE_PLAYER;
@@ -352,7 +420,7 @@ void iDraw() {
     } else if (app_state==STATE_GAME) {
         iSetColor(255,255,255);
         char pos[50];
-        snprintf(pos, 50, "%d, %d, %d",(int)player.pos.x, (int)player.pos.y, (int)player.pos.z);
+        snprintf(pos, 50, "%d, %d, %d",(int)player.km.pos.x, (int)player.km.pos.y, (int)player.km.pos.z);
         iText(10, 10, pos,GLUT_BITMAP_TIMES_ROMAN_24);
         // iGame();
         iDrawQueue();
@@ -431,10 +499,10 @@ void iKeyboard(unsigned char key) {
             case 'q': case ESC: app_state=STATE_MENU; break;
             case 'r': 
             {
-                player.pos.x=0;
-                player.pos.y=0;
-                player.pos.z=0; 
-                printf("%lf %lf %lf\n",player.pos.x,player.pos.y,player.pos.z); 
+                player.km.pos.x=0;
+                player.km.pos.y=0;
+                player.km.pos.z=0; 
+                printf("%lf %lf %lf\n",player.km.pos.x,player.km.pos.y,player.km.pos.z); 
                 break;
             }            
             default: break;
@@ -464,10 +532,10 @@ void iSpecialKeyboard(unsigned char key)
     } else if (app_state==STATE_GAME) {
         switch(key) {
             case GLUT_KEY_END: break;
-            case GLUT_KEY_LEFT: iPlayerMove(player.pos.x, player.pos.y,player.pos.z-1); break;
-            case GLUT_KEY_RIGHT: iPlayerMove(player.pos.x, player.pos.y,player.pos.z+1); break;
-            case GLUT_KEY_UP: iPlayerMove(player.pos.x-1, player.pos.y,player.pos.z); break;
-            case GLUT_KEY_DOWN: iPlayerMove(player.pos.x+1, player.pos.y,player.pos.z); break;
+            case GLUT_KEY_LEFT: iBodyMove(player.km.pos.x, player.km.pos.y,player.km.pos.z-1,&player.km); break;
+            case GLUT_KEY_RIGHT: iBodyMove(player.km.pos.x, player.km.pos.y,player.km.pos.z+1,&player.km); break;
+            case GLUT_KEY_UP: iBodyMove(player.km.pos.x-1, player.km.pos.y,player.km.pos.z,&player.km); break;
+            case GLUT_KEY_DOWN: iBodyMove(player.km.pos.x+1, player.km.pos.y,player.km.pos.z,&player.km); break;
             default: break;
         }
     }
